190eb203f24547d60aa0f29eceafee5d90b46660
Gian Carlo Pace <giancarlo.pace@gmail.com>
2010-03-26 14:53:25 +0100

    Fixed a Cached annotation that is not referable to a class anymore

--- src/org/riotfamily/example/cache/ClockController.java
+++ src/org/riotfamily/example/cache/ClockController.java
@@ -7,10 +7,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller("clock")
-@Cache(ttl="30s")
 public class ClockController {
 
 	@RequestMapping("/clock")
+	@Cache(ttl="30s")
 	public void handleRequest(Writer out) throws Exception {
 		out.write(String.valueOf(System.currentTimeMillis()));
 	}
